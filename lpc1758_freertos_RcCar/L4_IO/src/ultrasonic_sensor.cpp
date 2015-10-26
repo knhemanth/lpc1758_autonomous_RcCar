a8a37fe11565770573a7d215bb294d05821452cd
Akshay Vijaykumar <akshay.vijaykumar@sjsu.edu>
2015-10-25 20:19:30 -0700

    Added Code for Implementing Average Filtering of Ultrasonic Sensor Readings

--- lpc1758_freertos_RcCar/L4_IO/src/ultrasonic_sensor.cpp
+++ lpc1758_freertos_RcCar/L4_IO/src/ultrasonic_sensor.cpp
@@ -2,37 +2,47 @@
  * ultrasonic_sensor.cpp
  *
  *  Created on: Sep 10, 2015
- *      Author: Hemanth K N
+ *      Author: Hemanth K N and Akshay Vijaykumar
  */
 
 #include <stdio.h>
 #include "ultrasonic_sensor.hpp"
 #include "adc0.h"
 
-
+/* TogglePower
+ * This function allows the caller to turn the sensor on or off.
+ * */
 void UltrasonicSensor::TogglePower(bool power)
 {
-
     if ( power )  // If power on is requested and it is not already on
     {
         GpioPower.setHigh();
     }
-
     else         // If power off is requested and it is not already off
     {
         GpioPower.setLow();
     }
-
 }
 
+/* Recalibrate
+ * Performs a power cycle of the Sensor
+ * */
 void UltrasonicSensor::Recalibrate()
 {
     TogglePower(OFF);
     TogglePower(ON);
 }
 
-float UltrasonicSensor::GetRangeValue(void)
+/* ReadRange
+ * Return the Raw Range value obtained from Sensor
+ * */
+float UltrasonicSensor::ReadRange(void)
 {
+    /* The function does the following:
+     * Read the ADC Step value for a particular voltage
+     * Calculate the voltage value by multiplying with ADC Step Voltage
+     * The value is divided by voltage per inch.
+     * */
     float usonic_adc_reading = 0;
     float usonic_sensor_reading_v = 0;     // reading of sensor in volts
     float usonic_sensor_reading_inch = 0;     // reading of sensor in inches
@@ -41,6 +51,27 @@ float UltrasonicSensor::GetRangeValue(void)
     usonic_sensor_reading_v = ( usonic_adc_reading * ADC_VOLTAGE_PER_STEP );
     usonic_sensor_reading_inch = ( usonic_sensor_reading_v / USONIC_VOLTAGE_PER_INCH );
 
-    return usonic_sensor_reading_inch;
+    if(getFilterUsage())
+    {
+        /* Send this data to Filter */
+        avg_filter.addValue(usonic_sensor_reading_inch);
+    }
 
+    return usonic_sensor_reading_inch;
+}
+
+/* GetFilteredRangeValue
+ * Returns the Filtered range output using an average filter
+ * */
+
+float UltrasonicSensor::GetFilteredRangeValue(void)
+{
+    /* If Filter has been used for the sensor, the output of the filter will be read from here
+     * Else, this function returns 0 */
+
+    if(getFilterUsage())
+    {
+        return avg_filter.getValue();
+    }
+    return 0;
 }
--- lpc1758_freertos_RcCar/L4_IO/ultrasonic_sensor.hpp
+++ lpc1758_freertos_RcCar/L4_IO/ultrasonic_sensor.hpp
@@ -2,58 +2,216 @@
  * ultrasonic_sensor.hpp
  *
  *  Created on: Sep 10, 2015
- *      Author: Hemanth K N
+ *      Author: Hemanth K N and Akshay Vijaykumar
  */
 
 #ifndef L4_IO_ULTRASONIC_SENSOR_HPP_
 #define L4_IO_ULTRASONIC_SENSOR_HPP_
 
-#include "gpio.hpp"
-#include "LPC17xx.h"    // For Pin Select
-#include "singleton_template.hpp"  // Singleton Template
+/* MODULE USAGE :
+ * In the 10hz periodic task, Call US_FRONT.ReadRange();
+ * This is followed by US_FRONT.GetFilteredRangeValue());
+ * ReadRange would read the current value while GetFilteredRangeValue would
+ * provide a filtered value based on the past 5 range readings.
+ *
+ * Make sure to include ultrasonic_sensor.hpp in the period_callbacks.cpp file
+ * */
 
+
+#include "gpio.hpp"
+#include "LPC17xx.h"               // For Pin Select
+#include "singleton_template.hpp"  // Singleton Template
+#include "circular_buffer.hpp"
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "projdefs.h"
+#include "file_logger.h"
+
+
+/* Custom Debug Print Function */
+#if 0
+#define PRINTME(...) LOG_DEBUG(__VA_ARGS__)
+#else
+#define PRINTME(...)
+#endif
 
 #define USONIC_SUPPLY_VCC               ( 3.3 )
 #define USONIC_FRONTSENSOR_ADCCHANNEL   ( 4 )
 #define USONIC_VOLTAGE_PER_INCH         ( (float)( (float)USONIC_SUPPLY_VCC/512) )    // From datasheet in v
 #define ADC04_PINSELECT_VALUE           (  0x00000003 )
 #define ADC04_PINSELECT_SHIFT           ( 28 )
-#define ADC_VOLTAGE_PER_STEP            ( (float)( (float)3/4096 ) )    // 12-bit ADC has 4096 steps across 3000mVolts
+#define ADC_VOLTAGE_PER_STEP            ( (float)( (float) 3/4096 ) )    // 12-bit ADC has 4096 steps across 3000mVolts
 
 #define OFF                             ( false )
 #define ON                              ( true )
 
+#define OVERWRITE_OLD_DATA              ( true )
+
+#define FILTER_ACCESS_TIMEOUT           ( 1 )
+
+/* Sensor Filter Template
+ * Performs Average Filtering.
+ * This class can be inherited with other sensor classes for filtering
+ * */
+
+template <typename INTYPE, typename OUTTYPE>
+class Sensor_Filter
+{
+    private:
+        CircularBuffer <INTYPE> cBuf; /* Circular Buffer holding Sensor Values */
+        double currentAverage;
+        SemaphoreHandle_t filterAccess;
+        bool stableStream;
+
+        /* This function calculates the average of values in cBuf */
+        void updateAverage( INTYPE data)
+        {
+            double tempVal = 0;
+            float mulFactor = 0;
+            INTYPE oldElement = 0;
+
+            if( !stableStream )
+            {
+                /* Add data to Buffer */
+                cBuf.push_back(data);
+                mulFactor = 1/(double)(cBuf.size());
+
+                /* If number of elements in the buffer < total buffer capacity
+                 * Calculate the average using loops */
+
+                for(int i = 0; i < (int)cBuf.size(); i++)
+                {
+                    tempVal += cBuf[i];
+                }
+
+                tempVal = tempVal * mulFactor;
+
+                currentAverage = tempVal;
+
+                stableStream = (cBuf.size() == cBuf.capacity());
+            }
+            else
+            {
+                /* Efficient way of calculating average in case the buffer is full
+                 * Every calculation will update the current average value.
+                 * To get new average:
+                 * new_average =
+                 *         current_average +
+                 *         ( new_element - oldest_element ) * mulFactor
+                 *
+                 * The CircularBuffer at this point is Full
+                 * */
+
+                oldElement = cBuf.peek_front();
+                /* Adding new data Element */
+
+                cBuf.push_back(data, OVERWRITE_OLD_DATA);
+
+                /* We now have a pointer to the old element in the buffer */
+                tempVal = currentAverage;
+
+                tempVal = tempVal + ( (  data - oldElement ) / ( cBuf.capacity() ) );
+
+                currentAverage = tempVal;
+
+            }
+
+        }
+
+    public:
+
+        Sensor_Filter(uint32_t bufSize):
+            cBuf(bufSize),
+            currentAverage(0),
+            stableStream(false)
+
+        {
+            filterAccess = xSemaphoreCreateMutex();
+            /* Nothing to be done */
+        }
+
+        void addValue(INTYPE data)
+        {
+            if( xSemaphoreTake(filterAccess, FILTER_ACCESS_TIMEOUT) )
+            {
+                /*  Push Value into FIFO and update Average */
+                updateAverage(data);
+                xSemaphoreGive(filterAccess);
+            }
+            else
+            {
+                printf("Semaphore not available for writing to filter\n");
+            }
+        }
+
+        OUTTYPE getValue(void)
+        {
+            OUTTYPE tempVar = 0;
+
+            if( xSemaphoreTake(filterAccess, FILTER_ACCESS_TIMEOUT) )
+            {
+                /* Return the current Average Value */
+                tempVar = static_cast<OUTTYPE> (currentAverage);
+
+                xSemaphoreGive(filterAccess);
+            }
+
+            else
+            {
+                printf("Semaphore not available for reading from filter\n");
+            }
+
+            return tempVar;
+
+
+        }
+};
+
+/* Ultrasonic Sensor Class
+ * */
 class UltrasonicSensor
 {
     private:
         GPIO GpioPower;     /* GPIO pin used as the Power Supply to the Ultrasonic Sensor */
+        Sensor_Filter <double, double> avg_filter; /* Sensor Filter applied to Ultrasonic Sensor */
+        bool isFilterUsed; /* Holds information of usage of a filter */
 
     protected:
 
-        UltrasonicSensor(LPC1758_GPIO_Type Power_Pin):
-            GpioPower(Power_Pin)
+        UltrasonicSensor(LPC1758_GPIO_Type Power_Pin, bool filterOpt = false, int filterSize = 0):
+            GpioPower(Power_Pin),
+            avg_filter(filterSize),
+            isFilterUsed(filterOpt)
         {
             /* Configuring the GpioPower pin as Output */
             GpioPower.setAsOutput();
         }
 
-        virtual ~UltrasonicSensor()
-        {
-            /* Nothing to do*/
-        }
 
     public:
+        /* Status Variables */
+
+        inline bool getFilterUsage(void)
+        {
+            return isFilterUsed;
+        }
+
         virtual void TogglePower(bool power);             // Power On/Off the sensor
-        virtual void Recalibrate();                      // Recalibrate the sensor [ power cycle ]
-        virtual float GetRangeValue(void);     // Return range from the sensor
+        virtual void Recalibrate();                       // Recalibrate the sensor [ power cycle ]
+        virtual float ReadRange(void);                    // Return range from the sensor
+        virtual float GetFilteredRangeValue(void);        // Return range value from the sensor
+
 };
 
-
+/* Ultrasonic Sensor at Front
+ * Uses P2_0 to Power it on.
+ * Uses ADC0.4.
+ * */
 class UsonicFrontSensor : public UltrasonicSensor, public SingletonTemplate<UsonicFrontSensor>
 {
     private:
         UsonicFrontSensor():
-            UltrasonicSensor(P2_0)   // Interface front sensor power to PIO2.0
+            UltrasonicSensor(P2_0, true, 10)   /* Interface front sensor power to PIO2.0; Filtering True; 10 Elements */
             {
                 init();
             }
@@ -64,6 +222,7 @@ class UsonicFrontSensor : public UltrasonicSensor, public SingletonTemplate<Uson
              *  We have interfaced this sensor to ADC0.4
              *  Setting the corresponding PinMux
              */
+
             LPC_PINCON->PINSEL3 |= (ADC04_PINSELECT_VALUE << ADC04_PINSELECT_SHIFT);
 
             // Power on the sensor
@@ -74,5 +233,9 @@ class UsonicFrontSensor : public UltrasonicSensor, public SingletonTemplate<Uson
         friend class SingletonTemplate<UsonicFrontSensor>;
 
 };
+
+// Macro to get Singleton Instance
+#define US_FRONT UsonicFrontSensor::getInstance()
+
 
 #endif /* L4_IO_ULTRASONIC_SENSOR_HPP_ */
