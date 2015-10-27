7456d390d88fbb7356456fcda00e3d9d306bd641
Anush Shankar <anush.shankar92@gmail.com>
2015-10-27 01:03:20 -0700

    Bluetooth test code for reception and basic can

---
+++ lpc1758_freertos/L5_Application/main.cpp
@@ -0,0 +1,213 @@
+/*
+ *     SocialLedge.com - Copyright (C) 2013
+ *
+ *     This file is part of free software framework for embedded processors.
+ *     You can use it and/or distribute it as long as this copyright header
+ *     remains unmodified.  The code is free for personal use and requires
+ *     permission to use in a commercial product.
+ *
+ *      THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ *      OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ *      MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ *      I SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
+ *      CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ *
+ *     You can reach the author of this software at :
+ *          p r e e t . w i k i @ g m a i l . c o m
+ */
+
+/**
+ * @file
+ * @brief This is the application entry point.
+ * 			FreeRTOS and stdio printf is pre-configured to use uart0_min.h before main() enters.
+ * 			@see L0_LowLevel/lpc_sys.h if you wish to override printf/scanf functions.
+ *
+ */
+#include "tasks.hpp"
+#include "examples/examples.hpp"
+#include "uart3.hpp"
+#include "stdio.h"
+#include "semphr.h"
+#include "can.h"
+
+#define baud_rate 9600
+#define bt_task_mem 1000
+#define bt_rx_size 64
+#define bt_tx_size 64
+#define uart_test 0
+#define bt_data_len 8
+#define can_mssg_id 0x024
+#define can_baud_kbps 100
+
+
+SemaphoreHandle_t binary_sem = 0;
+can_msg_t tx_mssg;
+extern char rec_str[bt_data_len];
+bool can_init_stat = 0;
+/**
+ * The main() creates tasks or "threads".  See the documentation of scheduler_task class at scheduler_task.hpp
+ * for details.  There is a very simple example towards the beginning of this class's declaration.
+ *
+ * @warning SPI #1 bus usage notes (interfaced to SD & Flash):
+ *      - You can read/write files from multiple tasks because it automatically goes through SPI semaphore.
+ *      - If you are going to use the SPI Bus in a FreeRTOS task, you need to use the API at L4_IO/fat/spi_sem.h
+ *
+ * @warning SPI #0 usage notes (Nordic wireless)
+ *      - This bus is more tricky to use because if FreeRTOS is not running, the RIT interrupt may use the bus.
+ *      - If FreeRTOS is running, then wireless task may use it.
+ *        In either case, you should avoid using this bus or interfacing to external components because
+ *        there is no semaphore configured for this bus and it should be used exclusively by nordic wireless.
+ */
+can_void_func_t bus_off_callback()
+{
+//CAN_reset_bus(can1);
+return 0;
+}
+
+can_void_func_t data_ov()
+{
+return 0;
+}
+
+void can_init()
+{
+    can_init_stat = CAN_init(can2, can_baud_kbps, 512, 512, bus_off_callback(), data_ov());
+    CAN_bypass_filter_accept_all_msgs();
+    CAN_reset_bus(can2);
+    tx_mssg.msg_id = can_mssg_id;
+    tx_mssg.frame_fields.data_len = bt_data_len;
+
+}
+
+class bt_uart_task : public scheduler_task
+{
+    public:
+        bt_uart_task(uint8_t priority) :
+            scheduler_task("bt_uart_task", bt_task_mem, priority)
+        {
+        }
+
+        bool init(void)
+        {
+
+            Uart3 &bt_uart = Uart3::getInstance();
+            bt_uart.init(baud_rate, bt_rx_size, bt_tx_size);
+            return true;
+        }
+
+        bool run(void *p)
+        {
+           if(xSemaphoreTake(binary_sem, 100))
+           {
+           Uart3 &bt_uart = Uart3::getInstance();
+
+#if uart_test
+           char *bt_str;
+           bt_str = bt_uart.uart3_gets();
+           puts(bt_str);
+#endif
+
+           char *bt_str;
+           bt_str = bt_uart.uart3_gets();
+           for(int i = 0;i < bt_data_len; i++)
+           {
+               tx_mssg.data.bytes[i] = (uint8_t)(*(bt_str + i));
+               tx_mssg.data.bytes[i] -= 48;
+               printf("\nval at %d = %d",i,tx_mssg.data.bytes[i]);
+           }
+
+           if(can_init_stat)
+           {
+               CAN_tx(can2, &tx_mssg,portMAX_DELAY);
+           }
+           }
+           return true;
+        }
+};
+
+int main(void)
+{
+    /**
+     * A few basic tasks for this bare-bone system :
+     *      1.  Terminal task provides gateway to interact with the board through UART terminal.
+     *      2.  Remote task allows you to use remote control to interact with the board.
+     *      3.  Wireless task responsible to receive, retry, and handle mesh network.
+     *
+     * Disable remote task if you are not using it.  Also, it needs SYS_CFG_ENABLE_TLM
+     * such that it can save remote control codes to non-volatile memory.  IR remote
+     * control codes can be learned by typing the "learn" terminal command.
+     */
+    //create binary semaphore
+        vSemaphoreCreateBinary(binary_sem);
+
+    scheduler_add_task(new terminalTask(PRIORITY_HIGH));
+
+    /* Consumes very little CPU, but need highest priority to handle mesh network ACKs */
+    scheduler_add_task(new wirelessTask(PRIORITY_CRITICAL));
+
+    scheduler_add_task(new bt_uart_task(PRIORITY_CRITICAL));
+
+    /* The task for the IR receiver */
+    // scheduler_add_task(new remoteTask  (PRIORITY_LOW));
+
+    /* Your tasks should probably used PRIORITY_MEDIUM or PRIORITY_LOW because you want the terminal
+     * task to always be responsive so you can poke around in case something goes wrong.
+     */
+
+    /**
+     * This is a the board demonstration task that can be used to test the board.
+     * This also shows you how to send a wireless packets to other boards.
+     */
+    #if 0
+        scheduler_add_task(new example_io_demo());
+    #endif
+
+    /**
+     * Change "#if 0" to "#if 1" to enable examples.
+     * Try these examples one at a time.
+     */
+    #if 0
+        scheduler_add_task(new example_task());
+        scheduler_add_task(new example_alarm());
+        scheduler_add_task(new example_logger_qset());
+        scheduler_add_task(new example_nv_vars());
+    #endif
+
+    /**
+	 * Try the rx / tx tasks together to see how they queue data to each other.
+	 */
+    #if 0
+        scheduler_add_task(new queue_tx());
+        scheduler_add_task(new queue_rx());
+    #endif
+
+    /**
+     * Another example of shared handles and producer/consumer using a queue.
+     * In this example, producer will produce as fast as the consumer can consume.
+     */
+    #if 0
+        scheduler_add_task(new producer());
+        scheduler_add_task(new consumer());
+    #endif
+
+    /**
+     * If you have RN-XV on your board, you can connect to Wifi using this task.
+     * This does two things for us:
+     *   1.  The task allows us to perform HTTP web requests (@see wifiTask)
+     *   2.  Terminal task can accept commands from TCP/IP through Wifly module.
+     *
+     * To add terminal command channel, add this at terminal.cpp :: taskEntry() function:
+     * @code
+     *     // Assuming Wifly is on Uart3
+     *     addCommandChannel(Uart3::getInstance(), false);
+     * @endcode
+     */
+    #if 0
+        Uart3 &u3 = Uart3::getInstance();
+        u3.init(WIFI_BAUD_RATE, WIFI_RXQ_SIZE, WIFI_TXQ_SIZE);
+        scheduler_add_task(new wifiTask(Uart3::getInstance(), PRIORITY_LOW));
+    #endif
+
+    scheduler_start(); ///< This shouldn't return
+    return -1;
+}
