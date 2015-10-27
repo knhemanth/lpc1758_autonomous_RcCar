20e03b549c693521d53fba2cdc5438f5bf7d8277
Anush Shankar <anush.shankar92@gmail.com>
2015-10-27 01:22:21 -0700

    Bluetooth app module connect and data send

---
+++ Android/Anush/CONNECT_TO_FIXED_MAC_SEND/app/src/androidTest/java/top_gun_rc/connect_to_fixed_mac_send/ApplicationTest.java
@@ -0,0 +1,13 @@
+package top_gun_rc.connect_to_fixed_mac_send;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
