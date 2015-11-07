20a1dcd77f687a4ff45090cf3270da0f8ecb6197
divyad <divya.dodda5@gmail.com>
2015-11-07 01:08:39 -0800

    refined periodic callback for interrupts

--- lpc1758_freertos_RcCar/L5_Application/periodic_scheduler/period_callbacks.cpp
+++ lpc1758_freertos_RcCar/L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -27,42 +27,17 @@
  * For example, the 1000Hz take slot runs periodically every 1ms, and whatever you
  * do must be completed within 1ms.  Running over the time slot will reset the system.
  */
+#include <3ultrasonic_sensor_interrupts.hpp>
 #include <stdint.h>
-#include<ir.h>
 #include "io.hpp"
 #include "periodic_callback.h"
-#include "ultrasonic_sensor.hpp"
 #include "queue.h"
-#include "can_msg_id.h"
+//#include "can_msg_id.h"
 #include "can.h"
 #include "string.h"
 
 /// This is the stack size used for each of the period tasks
 const uint32_t PERIOD_TASKS_STACK_SIZE_BYTES = (512 * 4);
-
-// This enumeration matches the distance of the obstacle
-typedef enum {
-    nearest = 0,
-    near    = 1,
-    middle  = 2,
-    far     = 3,
-} distance_obstacle;
-
-char zoneMessage[4][10] = {"nearest", "near", "middle", "far"};
-
-//This enumeration matches the threshold of the obstacle
-typedef enum{
-    threshold_zero    = 0 ,
-    threshold_nearest = 50,
-    threshold_near    = 100,
-    threshold_middle  = 200,
-    thrshold_far      = 400,
-} obs_thre;
-
-//Divya editing master module for ultrasonic sensor
-//Creating a class instance of all the ultrasonic modules
-Ultra_Sonic_4ping front_ultrasonic(P2_0, P2_1);
-
 
 void period_1Hz(void)
 {
@@ -71,64 +46,14 @@ void period_1Hz(void)
 
 void period_10Hz(void)
 {
+    //      can_msg_t msg;
+    //      dist_sensor all_sensor;
 
     LE.toggle(2);
-}
 
-void period_100Hz(void)
-{
+    interrupt_based_ping_sensor();
 
-      float latest_value = 0;
-      float avg_distance_of_obstacle = 0;
-      distance_obstacle obstacle_zone;
-      obs_thre threshold;
-      can_msg_t msg;
-      dist_sensor all_sensor;
-
-    LE.toggle(3);
-
-    //Ping Sensor input
-     front_ultrasonic.send_trig();
-
-     if(front_ultrasonic.recieve_from_queue())  //if the queue has a value->returns true
-     {
-         latest_value = front_ultrasonic.get_buffer_value();
-
-
-         front_ultrasonic.add_queue_value_to_filter(latest_value);  //adding a float values in the filter
-
-         avg_distance_of_obstacle = front_ultrasonic.ping_get_from_filter(); //returns the latest average value of the distances
-
-
-         //printf("%f: \n",avg_distance_of_obstacle);
-     }
-     else
-     {
-         //printf("No values in the filter\n");
-     }
-
-    // front_ultrasonic.display();
-#if 1
-            if( (avg_distance_of_obstacle >= threshold_zero) && (avg_distance_of_obstacle <= threshold_nearest))
-            {
-                obstacle_zone = nearest;
-            }
-            else if((avg_distance_of_obstacle > threshold_nearest) && (avg_distance_of_obstacle <= threshold_near))
-            {
-                   obstacle_zone = near;
-            }
-            else if((avg_distance_of_obstacle > threshold_near) && (avg_distance_of_obstacle <= threshold_middle))
-                     {
-                            obstacle_zone = middle;
-                     }
-                else if((avg_distance_of_obstacle > threshold_middle) && (avg_distance_of_obstacle <= thrshold_far))
-                     {
-                            obstacle_zone = far;
-                     }
-
-            printf("Z[%s]\n", zoneMessage[obstacle_zone]);
-#endif
-         //sending the obstacle zone
+#if 0        //sending the obstacle zone
          all_sensor.front_center = obstacle_zone ;
 
          msg.msg_id = DISTANCE_SENSOR_ID ;
@@ -136,12 +61,17 @@ void period_100Hz(void)
          msg.frame_fields.data_len = sizeof(dist_sensor);
          memcpy(&msg.data.qword,&all_sensor,sizeof(dist_sensor));
          CAN_tx(can1,&msg,0);
+#endif
 
+}
+
+void period_100Hz(void)
+{
+    LE.toggle(3);
 }
 
 
 void period_1000Hz(void)
 {
     LE.toggle(4);
-
 }
